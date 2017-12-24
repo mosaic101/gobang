859362ba8a26c3196f490e4333d44f9a0b133d91
JianJin Wu <mosaic101@foxmail.com>
2017-12-24 16:00:23 +0800

    init

---
+++ src/components/Gobang.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class='hello'>
+    <h1>{{ msg }}</h1>
+    <canvas id='canvas'>
+      您的浏览器不支持canvas动画效果
+    </canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'gobang',
+  data () {
+    return {
+      msg: 'gobang game.'
+    }
+  },
+  mounted () {
+    let chess = document.getElementById('canvas')
+    let context = chess.getContext('2d')
+    chess.width = chess.height = 450
+    if (!context) console.log('浏览器不支持 canvas！')
+
+    let xStart, yStart, xEnd, yEnd
+    xStart = yStart = 15
+    xEnd = yEnd = 435
+
+    let me = true
+    // 用于存放棋盘中落子的情况, 初始值为 0
+    let chessBox = []
+    for (let i = 0; i < 15; i++) {
+      chessBox[i] = []
+      for (let j = 0; j < 15; j++) {
+        chessBox[i][j] = 0
+      }
+    }
+
+    // 绘制棋盘
+    function drawChessBoard () {
+      context.strokeStyle = '#D6D1D1'
+      for (let i = 0; i < 15; i++) {
+        // 绘制竖线
+        context.moveTo(xStart + i * 30, yStart)
+        context.lineTo(xStart + i * 30, yEnd)
+        context.stroke()
+        // 绘制横线
+        context.moveTo(xStart, yStart + i * 30)
+        context.lineTo(xEnd, yStart + i * 30)
+        context.stroke()
+      }
+    }
+    drawChessBoard()
+
+    function oneStep (i, j, k) {
+      context.beginPath()
+      // 绘制圆形棋子
+      context.arc(xStart + i * 30, yStart + j * 30, 13, 0, 2 * Math.PI)
+      // 设置渐变
+      let grd = context.createRadialGradient(xStart + i * 30, yStart + j * 30, 13, xStart + i * 30, yStart + j * 30, 0)
+
+      if (k) {
+        grd.addColorStop(0, '#0A0A0A') // 黑棋
+        grd.addColorStop(1, '#636766')
+      } else {
+        grd.addColorStop(0, '#D1D1D1') // 白棋
+        grd.addColorStop(1, '#F9F9F9')
+      }
+      context.fillStyle = grd
+      context.fill()
+      context.closePath()
+    }
+
+    chess.onclick = function (e) {
+      console.log(123123, chessBox)
+      let x = e.offsetX // 相对于棋盘左上角的x坐标
+      let y = e.offsetY // 相对于棋盘左上角的y坐标
+      let i = Math.floor(x / 30)
+      let j = Math.floor(y / 30)
+      if (chessBox[i][j] === 0) {
+        oneStep(i, j, me)
+        if (me) {
+          chessBox[i][j] = 1
+        } else {
+          chessBox[i][j] = 2
+        }
+        me = !me // 下一步白棋
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+  display: block;
+  margin: 50px auto;
+  box-shadow: -2px -2px 2px #F3F2F2, 5px 5px 5px #6F6767;
+}
+</style>
